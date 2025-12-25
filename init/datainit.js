1cbc5d90a802152e1ac2ab68df60676a9891b60f
Dixitha <ragidixitha1@gmail.com>
2025-12-25 14:12:42 +0530

    fix seed script for production

--- init/datainit.js
+++ init/datainit.js
@@ -1,33 +1,24 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const Listing = require("../models/listingM");
-const User = require("../models/users");
 const data = require("./data");
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/airbnb");
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("Connected to MongoDB Atlas");
 }
-
-main();
 
 const initDb = async () => {
   await Listing.deleteMany({});
-
-  const owner = await User.findOne({ username: "admin" });
-
-  if (!owner) {
-    console.log("No owner found. Run seedUser first.");
-    return;
-  }
-
-  const listingsWithOwner = data.data.map(listing => ({
-    ...listing,
-    owner: owner._id
-  }));
-
-  await Listing.insertMany(listingsWithOwner);
-  console.log("Listings seeded successfully");
-
-  mongoose.connection.close();
+  await Listing.insertMany(data.data);
+  console.log("Dummy listings inserted");
 };
 
-initDb();
+main()
+  .then(initDb)
+  .then(() => {
+    mongoose.connection.close();
+  })
+  .catch(err => {
+    console.error(err);
+  });
